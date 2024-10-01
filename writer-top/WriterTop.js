2aeb7baed5badd46e43957375bbe2d230a7edd15
kate-makarova <kate.makarova@hotmail.com>
2024-10-01 08:37:48 -0400

    --

--- writer-top/WriterTop.js
+++ writer-top/WriterTop.js
@@ -6,9 +6,23 @@ class WriterTop {
         this.posts = {}
     }
 
-    async execute() {
+    dateFormat(timestamp) {
+        var a = new Date(timestamp * 1000);
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours();
+        var min = a.getMinutes();
+        var sec = a.getSeconds();
+        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+        return time;
+    }
+
+    async execute(div_id) {
         await this.processTopics();
-        this.render();
+        console.log(this)
+        document.getElementById(div_id).innerHTML = this.render();
     }
 
     render() {
@@ -23,7 +37,7 @@ class WriterTop {
             const userPosts = this.posts[user['user_id']];
             html += `<div><span>${user['user_name']}</span><span>${user['count']}</span><span><ul>`;
             userPosts.forEach((post) => {
-                html += `<li><a href="/viewtopic.php?pid=${post['post_id']}">${post['subject']} (${post['posted']})</a></li>`
+                html += `<li><a href="/viewtopic.php?pid=${post['post_id']}">${post['subject']}</a> (${this.dateFormat(post['posted'])})</li>`
             })
             html += `</ul></span></div>`
         })
@@ -31,20 +45,23 @@ class WriterTop {
     }
 
     async processTopics() {
-        const topicData = this.forumIds.flat(async (forumId) => {
-            return await this.apiCall('topic.get', {"forum_id": forumId}, ["id", "init_post", "last_post_date"], 'last_post')
-        })
+        let topicData = await Promise.all(this.forumIds.map(async (forumId) => {
+            return await this.apiCall('topic.get', {"forum_id": forumId}, ["id", "init_post", "last_post_date"], 'last_post', 'desc')
+        }))
+        topicData =  await topicData.flat(1)
 
-        topicData.filter((topic) => {return parseInt(topic['last_post_date']) > this.startDate})
+        topicData = topicData.filter((topic) => {return parseInt(topic['last_post_date']) > this.startDate})
+        console.log(topicData);
 
-        await Promise.all(topicData.forEach(async (topicDatum) => {
+        await Promise.all(topicData.map(async (topicDatum) => {
             const result = await this.findPosts(topicDatum['id'], topicDatum['init_post']);
         }))
     }
 
     async findPosts(topicId, initPostId) {
         const postData = await this.apiCall('post.get', {"topic_id": topicId},
-            ["id", "user_id", "subject", "posted"], 'posted');
+            ["id", "user_id", "username", "subject", "posted"], 'posted', 'desc');
+        console.log(postData)
         let c = true;
         let i = 0;
         while (c && i < postData.length) {
@@ -82,14 +99,14 @@ class WriterTop {
      * @param fields - array
      * @param sortBy = string
      */
-    async apiCall(method, filters = null, fields = [], sortBy = null) {
-        const response = await fetch(this.combineUrl(method, filters, fields, sortBy))
+    async apiCall(method, filters = null, fields = [], sortBy = null, sortDir = null) {
+        const response = await fetch(this.combineUrl(method, filters, fields, sortBy, sortDir))
         const j = await response.json()
         return j['response']
-      //  return response.json()
+        //  return response.json()
     }
 
-    combineUrl(method, filters = null, fields = [], sortBy = null)
+    combineUrl(method, filters = null, fields = [], sortBy = null, sortDir = null)
     {
         let url = '/api.php?method='+method;
         if (filters) {
@@ -104,10 +121,11 @@ class WriterTop {
         if (sortBy) {
             url += '&sort_by='+sortBy;
         }
+
+        if (sortDir) {
+            url += '&sort_dir='+sortDir;
+        }
         return url;
     }
 
-
 }
-
-module.exports = WriterTop;
